303edaaa0c8a8705b013fa56a063c15a43e487b2
Bartosz Grabowski <bartek.grabowski.280@gmail.com>
2023-04-27 12:29:05 +0200

    Retriving data from api

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -4,30 +4,29 @@ import './App.css';
 import axios from 'axios';
 
 function App() {
-  const [books, setBooks] = useState();
+  const [books, setBooks] = useState([]);
 
+  //return our data from the api
   useEffect(() => {
     axios.get('http://localhost:5000/api/books')
       .then(response => {
+        console.log(response);
         setBooks(response.data);
       })
-  })
+  }, [])
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ul>
+          <p>THIS IS ONLY FOR TESTING PURPOUSE</p>
+          {books.map((book: any) => (
+            <li key={book.id}>
+              {book.title}
+            </li>
+          ))}
+        </ul>
       </header>
     </div>
   );
