ef7b1763494488be838a228616f455fcf3263045
Bartosz Grabowski <bartek.grabowski.280@gmail.com>
2023-04-29 14:43:21 +0200

    moved services to an extension method to clean up startup class

---
+++ Recobookation.API/Commons/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,34 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Recobookation.App.Books;
+using Recobookation.App.Core;
+using Recobookation.Persistence;
+
+namespace Recobookation.API.Commons.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddDbContext<DataContext>(options => options
+                .UseSqlServer(config.GetConnectionString("DefaultConnection")));
+
+            //cors policy allowing to safely retrive data returned by our api so the client-app can use the data
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
+
+            //Mediator to simplify handling queries
+            services.AddMediatR(typeof(BookList.Handler));
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            return services;
+        }
+    }
+}
--- Recobookation.API/Program.cs
+++ Recobookation.API/Program.cs
@@ -1,34 +1,14 @@
-using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Recobookation.App.Books;
-using Recobookation.App.Core;
+using Recobookation.API.Commons.Extensions;
 using Recobookation.Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
-var modelBuilder =
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<DataContext>(options => options
-    .UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
-//cors policy allowing to safely retrive data returned by our api so the client-app can use the data
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("CorsPolicy", policy =>
-    {
-        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-    });
-});
-
-//Mediator to simplify handling queries
-builder.Services.AddMediatR(typeof(BookList.Handler));
-builder.Services.AddAutoMapper(typeof(MappingProfiles).Assembly);
-
+builder.Services.AddApplicationServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
